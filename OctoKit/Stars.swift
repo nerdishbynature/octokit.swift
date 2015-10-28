e32bb09a06f64786f8fcf1f0f3ba799eb9c2e3af
Nate West <nwestr@gmail.com>
2015-10-28 10:33:00 -0400

    Starred Repositories

---
+++ OctoKit/Stars.swift
@@ -0,0 +1,71 @@
+//
+//  Stars.swift
+//  OctoKit
+//
+//  Created by Nate West on 10/27/15.
+//  Copyright © 2015 nerdish by nature. All rights reserved.
+//
+
+import Foundation
+
+public extension Octokit {
+    public func stars(name: String, completion: (response: Response<[Repository]>) -> Void) {
+        loadJSON(StarsRouter.ReadStars(name, self), expectedResultType: [[String: AnyObject]].self) { json, error in
+            if let error = error {
+                completion(response: Response.Failure(error))
+            } else {
+                if let json = json {
+                    let parsedStars = json.map { Repository($0) }
+                    completion(response: Response.Success(parsedStars))
+                }
+            }
+        }
+    }
+
+    public func myStars(completion: (response: Response<[Repository]>) -> Void) {
+        loadJSON(StarsRouter.ReadAuthenticatedStars(self), expectedResultType: [[String: AnyObject]].self) { json, error in
+            if let error = error {
+                completion(response: Response.Failure(error))
+            } else {
+                if let json = json {
+                    let parsedStars = json.map { Repository($0) }
+                    completion(response: Response.Success(parsedStars))
+                }
+            }
+        }
+    }
+}
+
+public enum StarsRouter: Router {
+    case ReadAuthenticatedStars(Octokit)
+    case ReadStars(String, Octokit)
+    public var method: HTTPMethod {
+        return .GET
+    }
+
+    public var encoding: HTTPEncoding {
+        return .URL
+    }
+
+    public var path: String {
+        switch self {
+        case .ReadAuthenticatedStars:
+            return "user/starred"
+        case .ReadStars(let username, _):
+            return "users/\(username)/starred"
+        }
+    }
+
+    public var params: [String: String] {
+        return [:]
+    }
+
+    public var URLRequest: NSURLRequest? {
+        switch self {
+        case .ReadAuthenticatedStars(let kit):
+            return kit.request(self)
+        case .ReadStars(_, let kit):
+            return kit.request(self)
+        }
+    }
+}
