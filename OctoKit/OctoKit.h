5d64ded2d2dddd1dbcf068325f4c879babc74e1a
Piet Brauer <piet@nerdishbynature.com>
2016-02-22 15:26:43 +0800

    Use Foundation instead of UIKit

--- OctoKit/OctoKit.h
+++ OctoKit/OctoKit.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 nerdish by nature. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for OctoKit.
 FOUNDATION_EXPORT double OctoKitVersionNumber;
