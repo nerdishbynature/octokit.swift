092da0b79f043d23c97364528b4d57bc239c2f5d
Piet Brauer <piet@nerdishbynature.com>
2016-02-22 15:52:11 +0800

    Remove @testable and only test public APIs

--- OctoKitTests/ConfigurationTests.swift
+++ OctoKitTests/ConfigurationTests.swift
@@ -1,6 +1,6 @@
 import XCTest
 import Foundation
-@testable import OctoKit
+import OctoKit
 import Nocilla
 
 class ConfigurationTests: XCTestCase {
@@ -39,23 +39,6 @@ class ConfigurationTests: XCTestCase {
         XCTAssertEqual(subject.token, "12345")
         XCTAssertEqual(subject.secret, "6789")
         XCTAssertEqual(subject.apiEndpoint, enterpriseURL)
-    }
-
-    func testAuthorizeURLRequest() {
-        let config = OAuthConfiguration(token: "12345", secret: "6789", scopes: ["repo", "read:org"])
-        let request = OAuthRouter.Authorize(config).URLRequest
-        let expected = NSURL(string: "https://github.com/login/oauth/authorize?client_id=12345&scope=repo%2Cread%3Aorg")!
-        XCTAssertEqual(request!.URL!, expected)
-    }
-
-    func testAccessTokenURLRequest() {
-        let config = OAuthConfiguration(token: "12345", secret: "6789", scopes: ["repo", "read:org"])
-        let request = OAuthRouter.AccessToken(config, "dhfjgh23493").URLRequest
-        let expected = NSURL(string: "https://github.com/login/oauth/access_token")!
-        let expectedBody = "client_id=12345&client_secret=6789&code=dhfjgh23493"
-        XCTAssertEqual(request!.URL!, expected)
-        let string = NSString(data: request!.HTTPBody!, encoding: NSUTF8StringEncoding)!
-        XCTAssertEqual(string as String, expectedBody)
     }
 
     func testAccessTokenFromResponse() {
