2cd0116c8ba7057a9922002790aaa3ebfa65cc52
Piet Brauer <piet@nerdishbynature.com>
2016-03-28 16:35:58 +0800

    Use TestSession

--- OctoKitTests/PublicKeyTests.swift
+++ OctoKitTests/PublicKeyTests.swift
@@ -1,41 +1,40 @@
 import XCTest
+import RequestKit
 import OctoKit
-import Nocilla
 
 class PublicKeyTests: XCTestCase {
-    override func setUp() {
-        super.setUp()
-        LSNocilla.sharedInstance().start()
-    }
-
-    override func tearDown() {
-        super.tearDown()
-        LSNocilla.sharedInstance().clearStubs()
-        LSNocilla.sharedInstance().stop()
-    }
-
     // MARK: Actual Request tests
 
     func testPostPublicKey() {
         let config = TokenConfiguration("12345")
-        if let json = Helper.stringFromFile("public_key") {
-            stubRequest("POST", "https://api.github.com/user/keys?access_token=12345").andReturn(201).withHeaders(["Content-Type": "application/json"]).withBody(json)
-            let expectation = expectationWithDescription("public_key")
-            Octokit(config).postPublicKey("test-key", title: "test title") { response in
-                switch response {
-                case .Success(let publicKey):
-                    XCTAssertEqual(publicKey, "test-key")
-                    expectation.fulfill()
-                case .Failure:
-                    XCTAssert(false, "should not get an error")
-                    expectation.fulfill()
-                }
+        let session = PublicKeyTestsSession()
+        Octokit(config).postPublicKey(session, publicKey: "test-key", title: "test title") { response in
+            switch response {
+            case .Success(let publicKey):
+                XCTAssertEqual(publicKey, "test-key")
+            case .Failure:
+                XCTAssert(false, "should not get an error")
             }
-            waitForExpectationsWithTimeout(1) { (error) in
-                XCTAssertNil(error, "\(error)")
-            }
-        } else {
-            XCTFail("json shouldn't be nil")
         }
+        XCTAssertTrue(session.wasCalled)
+    }
+}
+
+class PublicKeyTestsSession: RequestKitURLSession {
+    var wasCalled: Bool = false
+
+    func dataTaskWithRequest(request: NSURLRequest, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) -> URLSessionDataTaskProtocol {
+        XCTFail("This should not be called")
+        return MockURLSessionDataTask()
+    }
+
+    func uploadTaskWithRequest(request: NSURLRequest, fromData bodyData: NSData?, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) -> URLSessionDataTaskProtocol {
+        XCTAssertEqual(request.URL?.absoluteString, "https://api.github.com/user/keys?access_token=12345")
+        XCTAssertEqual(request.HTTPMethod, "POST")
+        let data = Helper.stringFromFile("public_key")?.dataUsingEncoding(NSUTF8StringEncoding)
+        let response = NSHTTPURLResponse(URL: request.URL!, statusCode: 201, HTTPVersion: "http/1.1", headerFields: ["Content-Type": "application/json"])
+        completionHandler(data, response, nil)
+        wasCalled = true
+        return MockURLSessionDataTask()
     }
 }
