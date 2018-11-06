fbf5b66e81f20d7063689be8e8c7c6623216ca06
Franco Meloni <franco.meloni91@gmail.com>
2018-11-06 18:41:11 +0000

    Remove not used Color extension

--- OctoKit/Label.swift
+++ OctoKit/Label.swift
@@ -1,25 +1,5 @@
-#if os(OSX)
-    import Cocoa
-    typealias Color = NSColor
-#elseif os(iOS) || os(tvOS) || os(watchOS)
-    import UIKit
-    typealias Color = UIColor
-#endif
+import Foundation
 
-extension Color {
-    @objc convenience init?(hexTriplet hex: String) {
-        var hexChars = Substring(hex)
-        let red = Int(String(hex.prefix(2)), radix: 16)
-        hexChars = hexChars.dropFirst(2)
-        let green = Int(String(hexChars.prefix(2)), radix: 16)
-        hexChars = hexChars.dropFirst(2)
-        let blue = Int(String(hexChars), radix: 16)
-        if let red = red, let green = green, let blue = blue {
-            self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1)
-        } else {
-            return nil
-        }
-    }
 }
 
 @objc open class Label: NSObject, Codable {
