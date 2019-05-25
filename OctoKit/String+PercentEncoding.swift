fba1fa8449d408816b7105a6bbe0e91a98ca0ced
Rob Mathers <robmathers@gmail.com>
2019-05-25 17:32:42 -0400

    Add a String extension to percent-encode for use in URLs

---
+++ OctoKit/String+PercentEncoding.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+extension String {
+    /// Percent-encodes a string to be URL-safe
+    ///
+    /// See https://useyourloaf.com/blog/how-to-percent-encode-a-url-string/ for more info
+    /// - returns: An optional string, with percent encoding to match RFC3986
+    func stringByAddingPercentEncodingForRFC3986() -> String? {
+        let unreserved = "-._~/?"
+        var allowed = CharacterSet.alphanumerics
+        allowed.insert(charactersIn: unreserved)
+        return addingPercentEncoding(withAllowedCharacters: allowed)
+    }
+}
