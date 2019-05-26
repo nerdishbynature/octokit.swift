fa5b487ed8b1f73a2f3069725db6828f5e755c76
Rob Mathers <robmathers@gmail.com>
2019-05-26 00:10:10 -0400

    Tests for .labels

--- Tests/OctoKitTests/LabelTests.swift
+++ Tests/OctoKitTests/LabelTests.swift
@@ -5,6 +5,8 @@ class LabelTests: XCTestCase {
     static var allTests = [
         ("testGetLabel", testGetLabel),
         ("testGetLabelEncodesSpaceCorrectly", testGetLabelEncodesSpaceCorrectly),
+        ("testGetLabels", testGetLabels),
+        ("testGetLabelsSetsPagination", testGetLabelsSetsPagination),
         ("testParsingLabel", testParsingLabel),
         ("testLinuxTestSuiteIncludesAllTests", testLinuxTestSuiteIncludesAllTests)
     ]
@@ -38,6 +40,34 @@ class LabelTests: XCTestCase {
         XCTAssertTrue(session.wasCalled)
     }
     
+    func testGetLabels() {
+        let session = OctoKitURLTestSession(expectedURL: "https://api.github.com/repos/octocat/hello-world/labels?page=1&per_page=100", expectedHTTPMethod: "GET", jsonFile: "labels", statusCode: 200)
+        let task = Octokit().labels(session, owner: "octocat", repository: "hello-world") { response in
+            switch response {
+            case .success(let labels):
+                XCTAssertEqual(labels.count, 7)
+            case .failure:
+                XCTAssert(false, "should not get an error")
+            }
+        }
+        XCTAssertNotNil(task)
+        XCTAssertTrue(session.wasCalled)
+    }
+    
+    func testGetLabelsSetsPagination() {
+        let session = OctoKitURLTestSession(expectedURL: "https://api.github.com/repos/octocat/hello-world/labels?page=2&per_page=50", expectedHTTPMethod: "GET", jsonFile: nil, statusCode: 200)
+        let task = Octokit().labels(session, owner: "octocat", repository: "hello-world", page: "2", perPage: "50") { response in
+            switch response {
+            case .success:
+                XCTAssert(true)
+            case .failure:
+                XCTAssert(false, "should not get an error")
+            }
+        }
+        XCTAssertNotNil(task)
+        XCTAssertTrue(session.wasCalled)
+    }
+
     
     // MARK: Parsing Tests
     func testParsingLabel() {
