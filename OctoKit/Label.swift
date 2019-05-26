5eaf37b4a59681fff3bb996cef66e4ba42b1f95e
Rob Mathers <robmathers@gmail.com>
2019-05-26 00:09:46 -0400

    Create method to get all labels in a repo

--- OctoKit/Label.swift
+++ OctoKit/Label.swift
@@ -31,10 +31,34 @@ public extension Octokit {
             }
         }
     }
+    
+    /**
+     Fetches all labels in a repository
+     - parameter session: RequestKitURLSession, defaults to URLSession.sharedSession()
+     - parameter owner: The user or organization that owns the repository.
+     - parameter repository: The name of the repository.
+     - parameter page: Current page for label pagination. `1` by default.
+     - parameter perPage: Number of labels per page. `100` by default.
+     - parameter completion: Callback for the outcome of the fetch.
+     */
+    @discardableResult
+    func labels(_ session: RequestKitURLSession = URLSession.shared, owner: String, repository: String, page: String = "1", perPage: String = "100", completion: @escaping (_ response: Response<[Label]>) -> Void) -> URLSessionDataTaskProtocol? {
+        let router = LabelRouter.readLabels(configuration, owner, repository, page, perPage)
+        return router.load(session, dateDecodingStrategy: .formatted(Time.rfc3339DateFormatter), expectedResultType: [Label].self) { labels, error in
+            if let error = error {
+                completion(Response.failure(error))
+            } else {
+                if let labels = labels {
+                    completion(Response.success(labels))
+                }
+            }
+        }
+    }
 }
 
 enum LabelRouter: JSONPostRouter {
     case readLabel(Configuration, String, String, String)
+    case readLabels(Configuration, String, String, String, String)
     var method: HTTPMethod {
         switch self {
         default:
@@ -58,6 +82,8 @@ enum LabelRouter: JSONPostRouter {
     var params: [String : Any] {
         switch self {
         case .readLabel: return [:]
+        case .readLabels(_, _, _, let page, let perPage):
+            return ["per_page": perPage, "page": page]
         }
     }
     
@@ -66,6 +92,8 @@ enum LabelRouter: JSONPostRouter {
         case .readLabel(_, let owner, let repository, let name):
             let name = name.stringByAddingPercentEncodingForRFC3986() ?? name
             return "/repos/\(owner)/\(repository)/labels/\(name)"
+        case .readLabels(_, let owner, let repository, _, _):
+            return "/repos/\(owner)/\(repository)/labels"
         }
     }
 }
