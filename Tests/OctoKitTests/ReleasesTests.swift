d3c8b723a8b2b86f2cf50331860513039c08a9fb
417-72KI <417.72ki@gmail.com>
2020-11-27 18:36:15 +0900

    add test case for list releases request

--- Tests/OctoKitTests/ReleasesTests.swift
+++ Tests/OctoKitTests/ReleasesTests.swift
@@ -11,11 +11,54 @@ import OctoKit
 
 final class ReleasesTests: XCTestCase {
     static var allTests = [
+        ("testListReleases", testListReleases),
         ("testPostRelease", testPostRelease),
         ("testLinuxTestSuiteIncludesAllTests", testLinuxTestSuiteIncludesAllTests)
     ]
 
     // MARK: Actual Request tests
+
+    func testListReleases() {
+        let session = OctoKitURLTestSession(expectedURL: "https://api.github.com/repos/octocat/Hello-World/releases", expectedHTTPMethod: "GET", jsonFile: "Fixtures/releases", statusCode: 200)
+        let task = Octokit().listReleases(session, owner: "octocat", repository: "Hello-World") {
+            switch $0 {
+            case let .success(releases):
+                XCTAssertEqual(releases.count, 2)
+                do {
+                    let release = try XCTUnwrap(releases.first)
+                    XCTAssertEqual(release.tagName, "v1.0.0")
+                    XCTAssertEqual(release.commitish, "master")
+                    XCTAssertEqual(release.name, "v1.0.0 Release")
+                    XCTAssertEqual(release.body, "The changelog of this release")
+                    XCTAssertFalse(release.prerelease)
+                    XCTAssertTrue(release.draft)
+                    XCTAssertNil(release.tarballURL)
+                    XCTAssertNil(release.zipballURL)
+                    XCTAssertNil(release.publishedAt)
+                } catch {
+                    XCTFail("Unwrap failed with error \(error)")
+                }
+                do {
+                    let release = try XCTUnwrap(releases.last)
+                    XCTAssertEqual(release.tagName, "v1.0.0")
+                    XCTAssertEqual(release.commitish, "master")
+                    XCTAssertEqual(release.name, "v1.0.0 Release")
+                    XCTAssertEqual(release.body, "The changelog of this release")
+                    XCTAssertFalse(release.prerelease)
+                    XCTAssertFalse(release.draft)
+                    XCTAssertNotNil(release.tarballURL)
+                    XCTAssertNotNil(release.zipballURL)
+                    XCTAssertNotNil(release.publishedAt)
+                } catch {
+                    XCTFail("Unwrap failed with error \(error)")
+                }
+            case let .failure(error):
+                XCTAssert(false, "Endpoint failed with error \(error)")
+            }
+        }
+        XCTAssertNotNil(task)
+        XCTAssertTrue(session.wasCalled)
+    }
 
     func testPostRelease() {
         let session = OctoKitURLTestSession(expectedURL: "https://api.github.com/repos/octocat/Hello-World/releases", expectedHTTPMethod: "POST", jsonFile: "post_release", statusCode: 201)
