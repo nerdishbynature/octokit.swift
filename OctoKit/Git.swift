e61178cbfec8d0d7f9d7f8a57a9c34ce64c6cc31
Antoine van der Lee <ajvanderlee@gmail.com>
2022-02-01 08:32:27 +0100

    Add `perPage` argument to list releases, delete release, delete reference endpoints (#137)
    * Allow configuring a limit for listing releases
    
    * Add a delete release command
    
    * Add delete reference endpoint
    
    * Update tests for releases per page
    
    * Run SwiftFormat
    
    * Run SwiftFormat after updating locally

---
+++ OctoKit/Git.swift
@@ -0,0 +1,75 @@
+//
+//  Git.swift
+//  OctoKit
+//
+//  Created by Antoine van der Lee on 25/01/2022.
+//  Copyright © 2020 nerdish by nature. All rights reserved.
+//
+
+import Foundation
+import RequestKit
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
+// MARK: request
+
+public extension Octokit {
+    /// Deletes a reference.
+    /// - Parameters:
+    ///   - session: RequestKitURLSession, defaults to URLSession.shared()
+    ///   - owner: The user or organization that owns the repositories.
+    ///   - repo: The repository on which the reference needs to be deleted.
+    ///   - ref: The reference to delete.
+    ///   - completion: Callback for the outcome of the deletion.
+    @discardableResult
+    func deleteReference(_ session: RequestKitURLSession = URLSession.shared,
+                         owner: String,
+                         repository: String,
+                         ref: String,
+                         completion: @escaping (_ response: Error?) -> Void) -> URLSessionDataTaskProtocol?
+    {
+        let router = GITRouter.deleteReference(configuration, owner, repository, ref)
+        return router.load(session, completion: completion)
+    }
+}
+
+// MARK: Router
+
+enum GITRouter: JSONPostRouter {
+    case deleteReference(Configuration, String, String, String)
+
+    var configuration: Configuration {
+        switch self {
+        case let .deleteReference(config, _, _, _): return config
+        }
+    }
+
+    var method: HTTPMethod {
+        switch self {
+        case .deleteReference:
+            return .DELETE
+        }
+    }
+
+    var encoding: HTTPEncoding {
+        switch self {
+        case .deleteReference:
+            return .url
+        }
+    }
+
+    var params: [String: Any] {
+        switch self {
+        case .deleteReference:
+            return [:]
+        }
+    }
+
+    var path: String {
+        switch self {
+        case let .deleteReference(_, owner, repo, reference):
+            return "repos/\(owner)/\(repo)/git/refs/\(reference)"
+        }
+    }
+}
