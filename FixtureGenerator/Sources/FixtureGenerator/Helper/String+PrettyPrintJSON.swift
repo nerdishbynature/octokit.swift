6e194c531c4af7a0fac672bf181dba7608ba43ff
Piet Brauer <piet@nerdishbynature.com>
2022-12-11 19:43:57 +0100

    Sort keys

--- FixtureGenerator/Sources/FixtureGenerator/Helper/String+PrettyPrintJSON.swift
+++ FixtureGenerator/Sources/FixtureGenerator/Helper/String+PrettyPrintJSON.swift
@@ -11,7 +11,7 @@ extension String {
     var prettyPrintedJSONString: String? {
         guard let dataValue = data(using: .utf8),
               let object = try? JSONSerialization.jsonObject(with: dataValue),
-              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted, .sortedKeys]),
               let prettyPrintedString = String(data: data, encoding: .utf8) else { return nil }
 
         return prettyPrintedString
