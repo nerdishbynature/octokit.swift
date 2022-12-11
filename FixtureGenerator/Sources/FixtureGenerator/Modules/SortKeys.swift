101d42b75ae1c84d4c6c6a22ba84bbf5b3364f73
Piet Brauer <piet@nerdishbynature.com>
2022-12-11 19:54:44 +0100

    Add sorted keys command

---
+++ FixtureGenerator/Sources/FixtureGenerator/Modules/SortKeys.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Piet Brauer-Kallenberg on 11.12.22.
+//
+
+import ArgumentParser
+import Foundation
+import Rainbow
+
+@available(macOS 12.0, *)
+struct SortedJSONKeys: AsyncParsableCommand {
+    public static let configuration = CommandConfiguration(
+        abstract: "Sorting Keys in JSON"
+    )
+
+    @Argument(help: "The file path to sort the JSON Content")
+    var filePaths: [String]
+
+    init() {}
+
+    mutating func run() async throws {
+        for filePath in filePaths {
+            let content = try String(contentsOfFile: filePath)
+            let prettyString = content.prettyPrintedJSONString
+            try prettyString?.write(toFile: filePath, atomically: true, encoding: .utf8)
+            print("Formatted \(filePath)".green)
+        }
+    }
+}
