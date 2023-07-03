d68e6d5c0fb3e51346279ae1948b53f75f1c1436
zunda <zunda.dev@gmail.com>
2023-07-03 16:49:29 +0900

    add init for Reactions

--- OctoKit/Reactions.swift
+++ OctoKit/Reactions.swift
@@ -13,6 +13,30 @@ public struct Reactions: Codable {
     public let rocket: Int?
     public let eyes: Int?
     public let url: String?
+  
+    public init(
+        totalCount: Int?,
+        thumbsUp: Int?,
+        thumbsDown: Int?,
+        laugh: Int?,
+        confused: Int?,
+        heart: Int?,
+        hooray: Int?,
+        rocket: Int?,
+        eyes: Int?,
+        url: String?
+    ) {
+        self.totalCount = totalCount
+        self.thumbsUp = thumbsUp
+        self.thumbsDown = thumbsDown
+        self.laugh = laugh
+        self.confused = confused
+        self.heart = heart
+        self.hooray = hooray
+        self.rocket = rocket
+        self.eyes = eyes
+        self.url = url
+    }
 
     enum CodingKeys: String, CodingKey {
         case laugh, confused, heart, hooray, url, eyes, rocket
