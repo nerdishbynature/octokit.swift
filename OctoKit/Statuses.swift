f01a343c018bc9ed08d421e960949687fcd88009
zunda <zunda.dev@gmail.com>
2023-07-03 16:39:58 +0900

    add init for Status

--- OctoKit/Statuses.swift
+++ OctoKit/Statuses.swift
@@ -18,6 +18,32 @@ open class Status: Codable {
     open var createdAt: Date?
     open var updatedAt: Date?
     open var creator: User?
+    
+    public init(
+        id: Int,
+        url: String? = nil,
+        avatarURL: String? = nil,
+        nodeId: String? = nil,
+        state: State? = nil,
+        description: String? = nil,
+        targetURL: String? = nil,
+        context: String? = nil,
+        createdAt: Date? = nil,
+        updatedAt: Date? = nil,
+        creator: User? = nil
+    ) {
+        self.id = id
+        self.url = url
+        self.avatarURL = avatarURL
+        self.nodeId = nodeId
+        self.state = state
+        self.description = description
+        self.targetURL = targetURL
+        self.context = context
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.creator = creator
+    }
 
     enum CodingKeys: String, CodingKey {
         case id
