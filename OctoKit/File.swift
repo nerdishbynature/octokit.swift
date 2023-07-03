ad1e377a8dca0bebf3cf7eaa0d7ed50d076d6c55
zunda <zunda.dev@gmail.com>
2023-07-03 16:48:09 +0900

    add init for File

--- OctoKit/File.swift
+++ OctoKit/File.swift
@@ -11,6 +11,24 @@ open class File: Codable {
     open var language: String?
     open var size: Int?
     open var content: String?
+  
+    public init(
+        id: Int,
+        rawURL: URL? = nil,
+        filename: String? = nil,
+        type: String? = nil,
+        language: String? = nil,
+        size: Int? = nil,
+        content: String? = nil
+    ) {
+        self.id = id
+        self.rawURL = rawURL
+        self.filename = filename
+        self.type = type
+        self.language = language
+        self.size = size
+        self.content = content
+    }
 
     enum CodingKeys: String, CodingKey {
         case rawURL = "raw_url"
