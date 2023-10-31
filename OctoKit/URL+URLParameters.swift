a50e71ebf3db6418271adf77bf2bc59dd450eee2
Piet Brauer <piet@nerdishbynature.com>
2023-10-31 10:03:55 +0100

    Update swiftformat

--- OctoKit/URL+URLParameters.swift
+++ OctoKit/URL+URLParameters.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-internal extension URL {
+extension URL {
     var URLParameters: [String: String] {
         guard let components = URLComponents(url: self, resolvingAgainstBaseURL: false) else { return [:] }
         var params = [String: String]()
