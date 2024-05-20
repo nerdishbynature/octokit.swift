d5f828ea2efe218eec171390569565929952a545
Łukasz Lech <l.lech@icloud.com>
2024-05-20 21:33:42 +0200

    Add milestone api

--- OctoKit/Milestone.swift
+++ OctoKit/Milestone.swift
@@ -1,4 +1,5 @@
 import Foundation
+import RequestKit
 
 open class Milestone: Codable {
     open var url: URL?
@@ -65,5 +66,285 @@ open class Milestone: Codable {
         case updatedAt = "updated_at"
         case closedAt = "closed_at"
         case dueOn = "due_on"
+    }
+}
+
+// MARK: Request
+
+public extension Octokit {
+    /**
+     Create a milestone
+     - parameter owner: The user or organization that owns the repositories.
+     - parameter repo: The name of the repository.
+     - parameter title: The title of the new milestone.
+     - parameter state: Filter pulls by their state.
+     - parameter description: The description of the new milestone.
+     - parameter date: The milestone due date
+     - parameter completion: Callback for the outcome of the create
+     */
+    @discardableResult
+    func createMilestone(owner: String,
+                         repo: String,
+                         title: String,
+                         state: Openness? = nil,
+                         description: String? = nil,
+                         dueDate: Date? = nil,
+                         completion: @escaping (_ response: Result<Milestone, Error>) -> Void) -> URLSessionDataTaskProtocol? {
+        let router = MilestoneRouter.createMilestone(configuration, owner, repo, title, state, description, dueDate)
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(Time.rfc3339DateFormatter)
+        return router.post(session, decoder: decoder, expectedResultType: Milestone.self) { milestone, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                if let milestone {
+                    completion(.success(milestone))
+                }
+            }
+        }
+    }
+    
+    /**
+     Get a single milestone
+     - parameter owner: The user or organization that owns the repositories.
+     - parameter repo: The name of the repository.
+     - parameter number: The number that identifies the milestone.
+     - parameter completion: Callback for the outcome of the fetch.
+     */
+    @discardableResult
+    func milestone(owner: String,
+                         repo: String,
+                         number: Int,
+                         completion: @escaping (_ response: Result<Milestone, Error>) -> Void) -> URLSessionDataTaskProtocol? {
+        let router = MilestoneRouter.readMilestone(configuration, owner, repo, number)
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(Time.rfc3339DateFormatter)
+        return router.post(session, decoder: decoder, expectedResultType: Milestone.self) { milestone, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                if let milestone {
+                    completion(.success(milestone))
+                }
+            }
+        }
+    }
+    
+    /**
+     Get a list of milestones
+     - parameter owner: The user or organization that owns the repositories.
+     - parameter repo: The name of the repository.
+     - parameter state: Filter pulls by their state.
+     - parameter direction: The direction of the sort.
+     - parameter page: The page to request.
+     - parameter perPage: The number of pulls to return on each page, max is 100.
+     - parameter completion: Callback for the outcome of the fetch.
+     */
+    @discardableResult
+    func milestones(owner: String,
+                    repo: String,
+                    state: Openness = .open,
+                    sort: SortType = .created,
+                    direction: SortDirection = .desc,
+                    page: Int? = nil,
+                    perPage: Int? = nil,
+completion: @escaping (_ response: Result<[Milestone], Error>) -> Void) -> URLSessionDataTaskProtocol? {
+        let router = MilestoneRouter.readMilestones(configuration, owner, repo, state, sort, direction, perPage, page)
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(Time.rfc3339DateFormatter)
+        return router.load(session, decoder: decoder, expectedResultType: [Milestone].self) { milestones, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                if let milestones {
+                    completion(.success(milestones))
+                }
+            }
+        }
+    }
+    
+    /**
+     Update a milestone
+     - parameter owner: The user or organization that owns the repositories.
+     - parameter repo: The name of the repository.
+     - parameter number: The number that identifies the milestone.
+     - parameter title: The title of the new milestone.
+     - parameter state: Filter pulls by their state.
+     - parameter description: The description of the new milestone.
+     - parameter date: The milestone due date
+     - parameter completion: Callback for the outcome of the update
+     */
+    @discardableResult
+    func updateMilestone(owner: String,
+                         repo: String,
+                         number: Int,
+                         title: String? = nil,
+                         state: Openness? = nil,
+                         description: String? = nil,
+                         dueDate: Date? = nil,
+                         completion: @escaping (_ response: Result<Milestone, Error>) -> Void) -> URLSessionDataTaskProtocol? {
+        let router = MilestoneRouter.updateMilestone(configuration, owner, repo, number, title, state, description, dueDate)
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(Time.rfc3339DateFormatter)
+        return router.post(session, decoder: decoder, expectedResultType: Milestone.self) { milestone, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                if let milestone {
+                    completion(.success(milestone))
+                }
+            }
+        }
+    }
+    /**
+     Delete a single milestone
+     - parameter owner: The user or organization that owns the repositories.
+     - parameter repo: The name of the repository.
+     - parameter number: The number that identifies the milestone.
+     - parameter completion: Callback for the outcome of the deletion.
+     */
+    @discardableResult
+    func deleteMilestone(owner: String,
+                         repo: String,
+                         number: Int,
+                         completion: @escaping (_ response: Error?) -> Void) -> URLSessionDataTaskProtocol? {
+        let router = MilestoneRouter.deleteMilestone(configuration, owner, repo, number)
+        return router.load(session, completion: completion)
+    }
+}
+
+// MARK: Router
+enum MilestoneRouter: Router, JSONPostRouter {
+    typealias Owner = String
+    typealias Repo = String
+    typealias Page = Int
+    typealias PerPage = Int
+    typealias Title = String
+    typealias Description = String
+    typealias MilestoneNumber = Int
+    
+    case readMilestones(Configuration, Owner, Repo, Openness, SortType, SortDirection, PerPage?, Page?)
+    case readMilestone(Configuration, Owner, Repo, MilestoneNumber)
+    case createMilestone(Configuration, Owner, Repo, Title, Openness?, Description?, Date?)
+    case updateMilestone(Configuration, Owner, Repo, MilestoneNumber, Title?, Openness?, Description?, Date?)
+    case deleteMilestone(Configuration, Owner, Repo, MilestoneNumber)
+    
+    var configuration: Configuration {
+        switch self {
+        case let .readMilestones(config, _, _, _, _, _, _, _): return config
+        case let .readMilestone(config, _, _, _): return config
+        case let .createMilestone(config, _, _, _, _, _, _): return config
+        case let .updateMilestone(config, _, _, _, _, _, _, _): return config
+        case let .deleteMilestone(config, _, _, _): return config
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .readMilestones, .readMilestone:
+            return .GET
+        case .createMilestone:
+            return .POST
+        case .updateMilestone:
+            return .PATCH
+        case .deleteMilestone:
+            return .DELETE
+        }
+    }
+    
+    var encoding: HTTPEncoding {
+        switch self {
+        case .readMilestones, .readMilestone, .deleteMilestone:
+            return .url
+        case .createMilestone, .updateMilestone:
+            return .json
+        }
+    }
+    
+    var params: [String: Any] {
+        switch self {
+        case let .readMilestones(_, _, _, state, sort, direction, page, perPage):
+            var parameters: [String: Any] = [
+                "state": state.rawValue,
+                "sort": sort.rawValue,
+                "direction": direction.rawValue
+            ]
+
+            if let page {
+                parameters["page"] = page
+            }
+
+            if let perPage {
+                parameters["per_page"] = perPage
+            }
+
+            return parameters
+            
+        case let .createMilestone(_, _, _, title, state, description, date):
+            var parameters: [String: Any] = [
+                "title": title
+            ]
+            
+            if let state {
+                if state == .all {
+                    parameters["state"] = Openness.open.rawValue
+                } else {
+                    parameters["state"] = state.rawValue
+                }
+            }
+            
+            if let description {
+                parameters["description"] = description
+            }
+            
+            if let date {
+                parameters["due_on"] = date
+            }
+            
+            return parameters
+            
+        case .readMilestone: return [:]
+            
+        case let .updateMilestone(_, _, _, _, title, state, description, date):
+            var parameters: [String: Any] = [:]
+            
+            if let title {
+                parameters["title"] = title
+            }
+            if let state {
+                if state == .all {
+                    parameters["state"] = Openness.open.rawValue
+                } else {
+                    parameters["state"] = state.rawValue
+                }
+            }
+            
+            if let description {
+                parameters["description"] = description
+            }
+            
+            if let date {
+                parameters["due_on"] = date
+            }
+            
+            return parameters
+            
+        case .deleteMilestone: return [:]
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case let .readMilestones(_, owner, repo, _, _, _, _, _):
+            return "repos/\(owner)/\(repo)/milestones"
+        case let .readMilestone(_, owner, repo, number):
+            return "repos/\(owner)/\(repo)/milestones/\(number)"
+        case let .createMilestone(_, owner, repo, _, _, _, _):
+            return "repos/\(owner)/\(repo)/milestones"
+        case let .updateMilestone(_, owner, repo, number, _, _, _, _):
+            return "repos/\(owner)/\(repo)/milestones/\(number)"
+        case let .deleteMilestone(_, owner, repo, number):
+            return "repos/\(owner)/\(repo)/milestones/\(number)"
+        }
     }
 }
---
+++ Tests/OctoKitTests/MilestoneTests.swift
@@ -0,0 +1,165 @@
+import OctoKit
+import XCTest
+
+class MilestoneTests: XCTestCase {
+    func testGetMilestone() {
+        let session = OctoKitURLTestSession(
+            expectedURL: "https://api.github.com/repos/octocat/Hello-World/milestones/1",
+            expectedHTTPMethod: "GET",
+            jsonFile: "milestone",
+            statusCode: 200
+        )
+        
+        let task = Octokit(session: session).milestone(
+            owner: "octocat",
+            repo: "Hello-World",
+            number: 1) { response in
+                switch response {
+                case let .success(milestone):
+                    XCTAssertEqual(milestone.id, 1002604)
+                    XCTAssertEqual(milestone.number, 1)
+                    XCTAssertEqual(milestone.title, "v1.0")
+                    XCTAssertEqual(milestone.url?.absoluteString, "https://api.github.com/repos/octocat/Hello-World/milestones/1")
+                   XCTAssertEqual(milestone.state, .open)
+                   XCTAssertEqual(milestone.milestoneDescription, "Tracking milestone for version 1.0")
+                    XCTAssertEqual(milestone.openIssues, 4)
+                    XCTAssertEqual(milestone.closedIssues, 8)
+                    XCTAssertEqual(milestone.creator?.login, "octocat")
+                    XCTAssertEqual(milestone.creator?.id, 1)
+                    XCTAssertEqual(milestone.creator?.type, "User")
+                    XCTAssertEqual(milestone.dueOn, Date(timeIntervalSince1970: 1349825941))
+                    
+                case let .failure(error):
+                    XCTFail(error.localizedDescription)
+                }
+            }
+        XCTAssertNotNil(task)
+        XCTAssertTrue(session.wasCalled)
+    }
+    
+    func testGetMilestones() {
+        let session = OctoKitURLTestSession(
+            expectedURL: "https://api.github.com/repos/octocat/Hello-World/milestones?direction=desc&sort=created&state=open",
+            expectedHTTPMethod: "GET",
+            jsonFile: "milestones",
+            statusCode: 200
+        )
+        
+        let task = Octokit(session: session).milestones(
+            owner: "octocat",
+            repo: "Hello-World") { response in
+                switch response {
+                case let .success(milestone):
+                    XCTAssertEqual(milestone.count, 1)
+                    let milestone = milestone.first!
+                    XCTAssertEqual(milestone.id, 1002604)
+                    XCTAssertEqual(milestone.number, 1)
+                    XCTAssertEqual(milestone.title, "v1.0")
+                    XCTAssertEqual(milestone.url?.absoluteString, "https://api.github.com/repos/octocat/Hello-World/milestones/1")
+                   XCTAssertEqual(milestone.state, .open)
+                   XCTAssertEqual(milestone.milestoneDescription, "Tracking milestone for version 1.0")
+                    XCTAssertEqual(milestone.openIssues, 4)
+                    XCTAssertEqual(milestone.closedIssues, 8)
+                    XCTAssertEqual(milestone.creator?.login, "octocat")
+                    XCTAssertEqual(milestone.creator?.id, 1)
+                    XCTAssertEqual(milestone.creator?.type, "User")
+                    XCTAssertEqual(milestone.dueOn, Date(timeIntervalSince1970: 1349825941))
+                    
+                case let .failure(error):
+                    XCTFail(error.localizedDescription)
+                }
+            }
+        XCTAssertNotNil(task)
+        XCTAssertTrue(session.wasCalled)
+    }
+    
+    func testCreateMilestone() {
+        let session = OctoKitURLTestSession(
+            expectedURL: "https://api.github.com/repos/octocat/Hello-World/milestones",
+            expectedHTTPMethod: "POST",
+            jsonFile: "milestone",
+            statusCode: 200
+        )
+        
+        let task = Octokit(session: session).createMilestone(
+            owner: "octocat",
+            repo: "Hello-World",
+            title: "v1.0") { response in
+                switch response {
+                case let .success(milestone):
+                    XCTAssertEqual(milestone.id, 1002604)
+                    XCTAssertEqual(milestone.number, 1)
+                    XCTAssertEqual(milestone.title, "v1.0")
+                    XCTAssertEqual(milestone.url?.absoluteString, "https://api.github.com/repos/octocat/Hello-World/milestones/1")
+                   XCTAssertEqual(milestone.state, .open)
+                   XCTAssertEqual(milestone.milestoneDescription, "Tracking milestone for version 1.0")
+                    XCTAssertEqual(milestone.openIssues, 4)
+                    XCTAssertEqual(milestone.closedIssues, 8)
+                    XCTAssertEqual(milestone.creator?.login, "octocat")
+                    XCTAssertEqual(milestone.creator?.id, 1)
+                    XCTAssertEqual(milestone.creator?.type, "User")
+                    XCTAssertEqual(milestone.dueOn, Date(timeIntervalSince1970: 1349825941))
+                    
+                case let .failure(error):
+                    XCTFail(error.localizedDescription)
+                }
+            }
+        XCTAssertNotNil(task)
+        XCTAssertTrue(session.wasCalled)
+    }
+    
+    func testUpdateMilestone() {
+        let session = OctoKitURLTestSession(
+            expectedURL: "https://api.github.com/repos/octocat/Hello-World/milestones/1",
+            expectedHTTPMethod: "PATCH",
+            jsonFile: "milestone",
+            statusCode: 200
+        )
+        
+        let task = Octokit(session: session).updateMilestone(
+            owner: "octocat",
+            repo: "Hello-World",
+            number: 1) { response in
+                switch response {
+                case let .success(milestone):
+                    XCTAssertEqual(milestone.id, 1002604)
+                    XCTAssertEqual(milestone.number, 1)
+                    XCTAssertEqual(milestone.title, "v1.0")
+                    XCTAssertEqual(milestone.url?.absoluteString, "https://api.github.com/repos/octocat/Hello-World/milestones/1")
+                   XCTAssertEqual(milestone.state, .open)
+                   XCTAssertEqual(milestone.milestoneDescription, "Tracking milestone for version 1.0")
+                    XCTAssertEqual(milestone.openIssues, 4)
+                    XCTAssertEqual(milestone.closedIssues, 8)
+                    XCTAssertEqual(milestone.creator?.login, "octocat")
+                    XCTAssertEqual(milestone.creator?.id, 1)
+                    XCTAssertEqual(milestone.creator?.type, "User")
+                    XCTAssertEqual(milestone.dueOn, Date(timeIntervalSince1970: 1349825941))
+                    
+                case let .failure(error):
+                    XCTFail(error.localizedDescription)
+                }
+            }
+        XCTAssertNotNil(task)
+        XCTAssertTrue(session.wasCalled)
+    }
+    
+    func testDeletMilestone() {
+        let session = OctoKitURLTestSession(
+            expectedURL: "https://api.github.com/repos/octocat/Hello-World/milestones/1",
+            expectedHTTPMethod: "DELETE",
+            jsonFile: "milestone",
+            statusCode: 200
+        )
+        
+        let task = Octokit(session: session).deleteMilestone(
+            owner: "octocat",
+            repo: "Hello-World",
+            number: 1) { response in
+                if let error = response {
+                    XCTFail(error.localizedDescription)
+                }
+            }
+        XCTAssertNotNil(task)
+        XCTAssertTrue(session.wasCalled)
+    }
+}
